9179f1beb61a5b7cbcb0b792dfafb24ea300e6fb
verystrongjoe <verystrongjoe@gmail.com>
2022-06-20 22:07:31 +0900

    add pytorch

---
+++ models.py
@@ -0,0 +1,176 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class Generator(nn.Module):
+    def __init__(self, config, input_dim=1):
+        super(Generator, self).__init__()
+        self.config = config
+        self.rnn_hid_size = config.G_hiddensize
+        self.seq_len = self.config.seq_len
+        self.input_dim = input_dim
+        self.device = torch.device(f'cuda:{config.gpu_num}' if torch.cuda.is_available() else 'cpu')
+
+        self.fwd_rnn_cell = nn.LSTMCell(input_dim, self.rnn_hid_size).to(self.device)
+        self.bwd_rnn_cell = nn.LSTMCell(input_dim, self.rnn_hid_size).to(self.device)
+        self.impute_reg = nn.Linear(self.rnn_hid_size, self.input_dim).to(self.device)
+        # self.out = nn.Linear(self.rnn_hid_size, self.input_dim).to(self.device)
+
+    def forward(self, data):
+        fwd_values = data['values']
+        fwd_masks = data['masks']
+
+        fwd_values = torch.unsqueeze(fwd_values, -1)
+        fwd_masks = torch.unsqueeze(fwd_masks, -1)
+        batch_size = fwd_values.size()[0]
+
+        assert fwd_values.shape == (batch_size, self.config.seq_len, 1)
+
+        bwd_values = torch.flip(fwd_values, (2,))
+        bwd_masks = torch.flip(fwd_masks, (2,))
+
+        fwd_start_value = Variable(torch.ones(batch_size, self.input_dim), requires_grad=False) * 128
+        bwd_start_value = Variable(torch.ones(batch_size, self.input_dim), requires_grad=False) * -128
+        fwd_start_value = fwd_start_value.to(self.device)
+        bwd_start_value = bwd_start_value.to(self.device)
+
+        h_fwd = Variable(torch.zeros((fwd_values.size()[0], self.rnn_hid_size)))  # (B, H)
+        c_fwd = Variable(torch.zeros((fwd_values.size()[0], self.rnn_hid_size)))  # (B, H)
+
+        h_bwd = Variable(torch.zeros((fwd_values.size()[0], self.rnn_hid_size)))  # (B, H)
+        c_bwd = Variable(torch.zeros((fwd_values.size()[0], self.rnn_hid_size)))  # (B, H)
+
+        if torch.cuda.is_available():
+            h_fwd, c_fwd, h_bwd, c_bwd = h_fwd.cuda(), c_fwd.cuda(), h_bwd.cuda(), c_bwd.cuda()
+
+        # append first identifier to data (B, T, 1)
+        # for the first time, we set first value as for equation(5).
+        h_fwd, c_fwd = self.fwd_rnn_cell(fwd_start_value, (h_fwd, c_fwd))
+        h_bwd, c_bwd = self.bwd_rnn_cell(bwd_start_value, (h_bwd, c_bwd))
+        imputations = []
+
+        for t in range(self.seq_len):
+            x_fwd = fwd_values[:, t, :]
+            m_fwd = fwd_masks[:, t, :]
+
+            x_bwd = bwd_values[:, t, :]
+            m_bwd = bwd_masks[:, t, :]
+
+            x_imputed_fwd = self.impute_reg(h_fwd)
+            x_imputed_bwd = self.impute_reg(h_bwd)
+
+            c_c_fwd = (1-m_fwd) * x_fwd + m_fwd * x_imputed_fwd
+            c_c_bwd = (1-m_bwd) * x_bwd + m_bwd * x_imputed_bwd
+
+            h_fwd, c_fwd = self.fwd_rnn_cell(c_c_fwd, (h_fwd, c_fwd))
+            h_bwd, c_bwd = self.bwd_rnn_cell(c_c_bwd, (h_bwd, c_bwd))
+            h = h_fwd + h_bwd
+
+            if t == (self.seq_len - 1):
+                imputations.append(h)
+
+        imputations = torch.cat(imputations, dim=1).reshape(batch_size, self.config.seq_len, self.input_dim)
+
+        # 원래 논문에는 1이 존재하는 데이터 0이 missing data임을 감안
+
+        return h, imputations * (1 - fwd_masks) + fwd_values *  fwd_masks
+
+
+class Decoder(nn.Module):
+    def __init__(self, config,  latent_dim, rnn_hid_size, output_dim, input_dim=1):
+        super(Decoder, self).__init__()
+        self.config = config
+        self.latent_dim = latent_dim
+        self.device = torch.device(f'cuda:{config.gpu_num}' if torch.cuda.is_available() else 'cpu')
+
+        self.rnn_cell = nn.LSTMCell(latent_dim, rnn_hid_size).to(self.device)
+        self.rnn_output = nn.Linear(rnn_hid_size, output_dim).to(self.device)
+
+        self.rnn_hid_size = rnn_hid_size
+        self.input_dim = input_dim
+        self.seq_len = config.seq_len
+
+    def forward(self, x):  # bi-rnn -> forward states + backward states
+        batch_size = x.size()[0]
+
+        # todo : 여기 start value를 어떻게 정의해야하나?
+        start_value = Variable(torch.ones(batch_size, self.latent_dim), requires_grad=False) * 128
+        start_value = start_value.to(self.device)
+
+        h = Variable(torch.zeros((batch_size, self.rnn_hid_size))).to(self.device)  # (B, H)
+        c = Variable(torch.zeros((batch_size, self.rnn_hid_size))) .to(self.device) # (B, H)
+
+        if torch.cuda.is_available():
+            h, c = h.cuda(), c.cuda()
+
+        h, c = self.rnn_cell(start_value, (h, c))
+        outputs = []
+
+        for t in range(self.seq_len):
+            h, c = self.rnn_cell(c, (h, c))
+            outputs.append(self.rnn_output(h))
+        return torch.stack(outputs, dim=1)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, config, input_dim=1):
+        super(Discriminator, self).__init__()
+        self.config = config
+        self.device = torch.device(f'cuda:{self.config.gpu_num}' if torch.cuda.is_available() else 'cpu')
+        self.output_layer = nn.Linear(32, input_dim).to(self.device)
+        self.disc_cells = nn.ModuleList(
+            [nn.LSTMCell(input_size=i, hidden_size=h)for (i, h) in
+             zip([input_dim, 32, 16, 8, 16], [32, 16, 8, 16, 32])
+             ])
+        self.disc_cells.to(self.device)
+
+    def forward(self, x):
+        outputs = []
+        # B, T, 1 -> B, T, H_out
+        for t in range(x.shape[1]):  # time seq
+            input = x[:, t, :]  # b, t, h    첫번째 b, h  32, 1
+            for i, rnn_cell in enumerate(self.disc_cells):
+                if i == 0:
+                    (h, c) = rnn_cell(input)
+                else:
+                    (h, c) = rnn_cell(h)
+            outputs.append(h)
+        return self.output_layer(torch.stack(outputs, dim=1))
+
+
+class CRLI(nn.Module):
+    def __init__(self, config):
+        rnn_hid_size = config.G_hiddensize
+        latent_dim = 16
+        output_dim = 1
+
+        super(CRLI, self).__init__()
+        self.config = config
+        self.device = torch.device(f'cuda:{config.gpu_num}' if torch.cuda.is_available() else 'cpu')
+
+        # upper branch
+        self.generator = Generator(config=config)
+        self.discriminator = Discriminator(self.config)
+
+        # lower branch
+        self.fully_connected = nn.Linear(rnn_hid_size, latent_dim).to(self.device)
+        self.decoder = Decoder(config, latent_dim, rnn_hid_size, output_dim).to(self.device)
+
+        ## third
+        f = torch.empty(config.batch_size, config.k_cluster, dtype=torch.float32)
+        torch.nn.init.orthogonal_(f, gain=1)
+        self.F = torch.autograd.Variable(f, requires_grad=False).to(self.device)
+
+
+    def forward(self, x):
+        h, imputed = self.generator(x)
+        disc_output = self.discriminator(imputed)
+        latent = self.fully_connected(h)
+        reconstructed = self.decoder(latent)
+        return imputed, disc_output, latent, reconstructed
+
+    def update_F(self, F_new):
+        self.F = F_new
+
+
---
+++ runner.py
@@ -0,0 +1,276 @@
+import torch
+from sklearn import metrics
+import pandas as pd
+import warnings
+import argparse
+import numpy as np
+from sklearn.cluster import KMeans
+import torch.nn as nn
+from scipy.optimize import linear_sum_assignment
+from models import CRLI
+import torch.nn.functional as F
+
+
+warnings.filterwarnings("ignore")
+parser = argparse.ArgumentParser()
+
+
+def mse_error(pred, target):
+    return nn.MSELoss()(pred, target)
+
+
+def pur_metric(pred, target):
+    n = len(pred)
+    tmp = pd.crosstab(pred, target)
+    tmp = np.array(tmp)
+    ret = np.max(tmp, 1)
+    ret = float(np.sum(ret))
+    ret = ret / n
+    return ret
+
+
+def nmi_metric(pred, target):
+    NMI = metrics.normalized_mutual_info_score(pred, target)
+    return NMI
+
+
+def RI_metric(pred, target):
+    # RI
+    n = len(target)
+    TP = 0
+    TN = 0
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if target[i] != target[j]:
+                if pred[i] != pred[j]:
+                    TN += 1
+            else:
+                if pred[i] == pred[j]:
+                    TP += 1
+
+    RI = n * (n - 1) / 2
+    RI = (TP + TN) / RI
+    return RI
+
+
+def _accuracy(y_pred, y_true):
+    def cluster_acc(Y_pred, Y):
+        assert Y_pred.size == Y.size
+        D = max(Y_pred.max(), Y.max()) + 1
+        w = np.zeros((D, D), dtype=np.int32)
+        for i in range(Y_pred.size):
+            w[Y_pred[i], Y[i]] += 1
+        ind = linear_sum_assignment(w.max() - w)
+        return sum([w[i, j] for i, j in ind]) * 1.0 / Y_pred.size, np.array(w)
+
+    y_pred = np.array(y_pred, np.int32)
+    y_true = np.array(y_true, np.int32)
+    return cluster_acc(y_pred, y_true)
+
+
+def assess(pred, target):
+    pur = pur_metric(pred, target)
+    nmi = nmi_metric(pred, target)
+    ri = RI_metric(pred, target)
+    acc, o = _accuracy(pred, target)
+    return ri, nmi, acc, pur
+
+
+def load_mask(filename):
+    mask_label = np.loadtxt(filename,delimiter = ",")
+    mask = mask_label[:,1:].astype(np.float32)
+    return mask
+
+def load_data(filename):
+    data_label = np.loadtxt(filename,delimiter = ",")
+    data = data_label[:,1:].astype(np.float32)
+    label = data_label[:,0].astype(np.int32)
+    return data,label
+
+def load_length(filename):
+    length = np.loadtxt(filename,delimiter = ",")
+    return length
+
+def load_lengthmark(filename):
+    lengthmark = np.loadtxt(filename,delimiter = ",")
+    return lengthmark
+
+
+def get_batch(data, label, mask, length, lengthmark, config):
+    samples_num = data.shape[0]
+    batch_num = int(samples_num / config.batch_size)
+    left_row = samples_num - batch_num * config.batch_size
+    device = torch.device(f'cuda:{config.gpu_num}' if torch.cuda.is_available() else 'cpu')
+
+    data = torch.from_numpy(data).to(device)
+    mask = torch.Tensor(mask).to(device)
+
+    for i in range(batch_num):
+        batch_data = data[i * config.batch_size: (i + 1) * config.batch_size, :]
+        batch_label = label[i * config.batch_size: (i + 1) * config.batch_size]
+        batch_mask = mask[i * config.batch_size: (i + 1) * config.batch_size, :]
+        batch_length = length[i * config.batch_size: (i + 1) * config.batch_size]
+        batch_lengthmark = lengthmark[i * config.batch_size: (i + 1) * config.batch_size,:]
+        yield (batch_data,batch_label,batch_mask,batch_length,batch_lengthmark)
+
+    if left_row != 0:
+        need_more = config.batch_size - left_row
+        need_more = np.random.choice(np.arange(samples_num), size=need_more)
+        batch_data = torch.concat((data[-left_row:, :], data[need_more,:]), axis=0)
+        batch_label = np.concatenate((label[-left_row:],label[need_more]), axis=0)
+        batch_mask = torch.concat((mask[-left_row:, :], mask[need_more,:]), axis=0)
+        batch_length = np.concatenate((length[-left_row:],length[need_more]), axis=0)
+        batch_lengthmark = np.concatenate((lengthmark[-left_row:,:],lengthmark[need_more,:]), axis=0)
+        yield (batch_data,batch_label,batch_mask,batch_length,batch_lengthmark)
+
+
+
+def run(config):
+    '''data'''
+    train_data_filename = config.data_dir + "/" + config.dataset_name + "/" + config.dataset_name + "_TRAIN"
+    train_data, train_label = load_data(train_data_filename)
+    test_data_filename = train_data_filename.replace('TRAIN', 'TEST')
+    test_data, test_label = load_data(test_data_filename)
+
+    '''mask'''
+    train_maskname = config.data_dir + "/" + config.dataset_name + "/" + "mask_" + config.dataset_name + "_TRAIN"
+    train_mask = load_mask(train_maskname)
+    test_maskname = train_maskname.replace('TRAIN', 'TEST')
+    test_mask = load_mask(test_maskname)
+
+    '''length'''
+    train_length_filename = config.data_dir + "/" + config.dataset_name + "/" + 'length_' + config.dataset_name + "_TRAIN"
+    train_length = load_length(train_length_filename)
+    test_length_filename = train_length_filename.replace('TRAIN', 'TEST')
+    test_length = load_length(test_length_filename)
+
+    '''train_lengthmark'''
+    train_lengthmark_filename = config.data_dir + "/" + config.dataset_name + "/" + 'lengthmark_' + config.dataset_name + "_TRAIN"
+    train_lengthmark = load_lengthmark(train_lengthmark_filename)
+    test_lengthmark_filename = train_lengthmark_filename.replace('TRAIN', 'TEST')
+    test_lengthmark = load_length(test_lengthmark_filename)
+
+    train_dataset_size = train_data.shape[0]
+    test_dataset_size = test_data.shape[0]
+    config.inputdims = 1
+    config.n_steps = train_data.shape[1] // config.inputdims
+    config.k_cluster = len(np.unique(train_label))
+    config.train_dataset_size = train_dataset_size
+
+    if config.batch_size > train_dataset_size:
+        config.batch_size = train_dataset_size
+    elif config.batch_size < config.k_cluster:
+        config.batch_size = config.k_cluster
+
+    RI = []
+    NMI = []
+    ACC = []
+    PUR = []
+    GLOBAL_STEP = -1
+
+    m = CRLI(config)
+
+    for i in range(config.epoch):
+        '''Train'''
+        for batch_data, batch_label, batch_mask, batch_length, batch_lengthmark in get_batch(
+                train_data, train_label, train_mask, train_length, train_lengthmark, config):
+            GLOBAL_STEP += 1
+            # batch_data (32,16)
+            # batch_label (32, )  filled with 1 or 0
+            # bath length (32, )  filled with 16
+            # batch lengthmark (32, 16)
+            # batch mask (32,16)
+            data = {}
+            data['values'] = batch_data
+            data['masks'] = batch_mask
+            for _ in range(config.D_steps):
+                imputed, disc_output, latent, reconstructed = m(data)
+                # run disc loss
+                loss_D = F.binary_cross_entropy_with_logits(disc_output.squeeze(), batch_mask).mean()
+                # backward
+
+            for j in range(config.G_steps):
+                # run gen
+                imputed, disc_output, latent, reconstructed = m(data)
+                # loss_G + loss_pre + loss_re + loss_km * config.lambda_kmeans
+                loss_G = F.binary_cross_entropy_with_logits(disc_output.squeeze(), 1-batch_mask).mean()
+
+                # loss_pre
+                pre_tmp = imputed.squeeze() * batch_mask
+                target_pre = batch_mask * batch_data
+                loss_pre = nn.MSELoss()(pre_tmp, target_pre)
+
+                # loss_re
+                out_tmp = reconstructed.squeeze() * batch_mask
+                targ_re = batch_data * batch_mask
+                # loss_re = mse_error(out_tmp, targ_re)
+                loss_re = nn.MSELoss()(out_tmp, targ_re)
+
+                # loss_km
+                HTH = torch.matmul(latent, latent.T)
+                FTHTHF = torch.matmul(torch.matmul(m.F.T, HTH), m.F)
+                loss_km = torch.trace(HTH) - torch.trace(FTHTHF)
+
+                if i % config.T_update_F == 0:
+                    '''calculate F'''
+                    # run H
+                    imputed, disc_output, latent, reconstructed = m(data)
+                    # F_update
+                    U, s, V = torch.linalg.svd(latent)
+                    F_new = U.T[:config.k_cluster, :]
+                    F_new = F_new.T
+                    m.update_F(F_new)
+
+            '''TEST'''
+            H_outputs = []
+            for batch_data, batch_label, batch_mask, batch_length, batch_lengthmark in get_batch(test_data,test_label,test_mask,test_length,test_lengthmark,config):
+                data = {}
+                data['values'] = batch_data
+                data['masks'] = batch_mask
+                # get outputs["H"]
+                imputed, disc_output, latent, reconstructed = m(data)
+                H_outputs.append(latent)
+            H_outputs = torch.concat(H_outputs, 0)
+            H_outputs = H_outputs[:test_dataset_size, :]
+            Km = KMeans(n_clusters=config.k_cluster)
+            pred_H = Km.fit_predict(H_outputs.cpu().detach().numpy())
+
+            '''record'''
+            ri,nmi,acc,pur = assess(pred_H,test_label)
+            RI.append(ri)
+            NMI.append(nmi)
+            ACC.append(acc)
+            PUR.append(pur)
+
+
+    ri = max(RI[80], RI[300], RI[500])
+    nmi = max(NMI[80], NMI[300], NMI[500])
+    acc = max(ACC[80], ACC[300], ACC[500])
+    pur = max(PUR[80], PUR[300], PUR[500])
+
+    return ri, nmi, pur, acc
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, required=False, default=32)
+    parser.add_argument('--T_update_F', type=int, required=False, default=1)
+    parser.add_argument('--G_steps', type=int, required=False, default=1)
+    parser.add_argument('--D_steps', type=int, required=False, default=1)
+    parser.add_argument('--epoch', type=int, required=False, default=500)
+    parser.add_argument('--learning_rate', type=float, required=False, default=5e-3)
+    parser.add_argument('--dataset_name', type=str, required=False, default='HouseVote')
+    parser.add_argument('--lambda_kmeans', type=float, required=False, default=1e-3)
+    parser.add_argument('--G_hiddensize', type=int, required=False, default=16)
+    parser.add_argument('--G_layer', type=int, required=False, default=1)
+    parser.add_argument('--IsD', type=bool, required=False, default=False)
+    parser.add_argument('--cell_type', type=str, required=False, default='gru')
+    parser.add_argument('--data_dir', type=str, required=False, default='dataset')
+    parser.add_argument('--seq_len', type=int, required=False, default=16)
+    parser.add_argument('--gpu_num', type=int, required=False, default=0)
+
+    config = parser.parse_args()
+    ri, nmi, pur, cluster_acc = run(config)
+
+    print('%s,%.6f,%.6f,%.6f,%.6f' % (config.dataset_name, ri, nmi, pur, cluster_acc))
+
